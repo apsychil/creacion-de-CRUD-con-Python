ec913501173a336f0f7dab634cc75133cf9d7f48
Mateo <mateomisas97@gmail.com>
2023-04-07 22:01:49 -0500

    nuevo commit

--- clients/commands.py
+++ clients/commands.py
@@ -1,6 +1,7 @@
 import click
 from clients.services import ClientService
 from clients.models import Client
+from tabulate import tabulate
 
 @click.group()
 def clients():
@@ -38,15 +39,52 @@ def create(ctx, name, company, email, position):
 @click.pass_context    
 def list(ctx):
     """List all clients"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+    clients_list = client_service.list_clients()
+
+    headers = [field.capitalize() for field in Client.schema()]
+    table = []
+
+    for client in clients_list:
+        table.append(
+            [client['name'],
+             client['company'],
+             client['email'],
+             client['position'],
+             client['uid']])
+
+    print(tabulate(table, headers))
 
 
 @clients.command()
+@click.argument('client_uid', type=str)
 @click.pass_context
 def update(ctx, client_uid):
     """Updates a client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+    
+    client_list = client_service.list_clients()
+    
+    client = [client for client in client_list if client['uid'] == client_uid]
+    
+    if client:
+        client = _update_client_flow(Client(**client[0]))
+        client_service.update_client(client)
+        
+        click.echo('Client updated')
+    else:
+        click.echo('Client not found')
 
+
+def _update_client_flow(client):
+    click.echo('Leave empty if you dont want to modify the value')
+    
+    client.name = click.prompt('New name', type=str, default=client.name)
+    client.company = click.prompt('New company', type=str, default=client.company)
+    client.email = click.prompt('New email', type=str, default=client.email)
+    client.position = click.prompt('New position', type=str, default=client.position)
+
+    return client
 
 @clients.command()
 @click.pass_context
